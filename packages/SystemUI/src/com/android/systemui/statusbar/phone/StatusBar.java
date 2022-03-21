f2752fe90c437e06380f1fccf63a2e77fdeb69c2
Rasenkai <rasenkai99@gmail.com>
2022-03-21 19:11:59 +0530

    Revert "SystemUI: Skip screen-on animation in all scenarios"
    * if u can't wait 0.5 seconds, u should kys
    
    This reverts commit b9317198cbbab4bf7040dd29d6cfacb0e6a515ff.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3757,7 +3757,8 @@ public class StatusBar extends SystemUI implements
 
         boolean launchingAffordanceWithPreview =
                 mNotificationPanelViewController.isLaunchingAffordanceWithPreview();
-        mScrimController.setLaunchingAffordanceWithPreview(true);
+        mScrimController.setLaunchingAffordanceWithPreview(launchingAffordanceWithPreview
+                || mBiometricUnlockController.isWakeAndUnlock());
 
         if (mStatusBarKeyguardViewManager.isShowingAlternateAuth()) {
             if (mState == StatusBarState.SHADE || mState == StatusBarState.SHADE_LOCKED) {
